2c7520a6d04ed227db3d598b8fd1a9399f174005
SWW <sylwia.werbinska@pwr.edu.pl>
2023-12-12 12:59:17 +0100

    uzupełnione

--- 08_Krotki/krotki.py
+++ 08_Krotki/krotki.py
@@ -5,8 +5,8 @@ t = 1, 2, 3, 4
 t
 t[1] = 19  # Błąd
 
-t = tuple(range(5))
-t
+
+
 t = tuple([1, 2, 3, 4])
 t
 
--- 11_Listy_i_zbiory-zaawansowane_mechanizmy/list_comprehension.py
+++ 11_Listy_i_zbiory-zaawansowane_mechanizmy/list_comprehension.py
@@ -14,9 +14,9 @@ list(range(10))
 
 [(x, y, x * y) for x in range(3) for y in range(4)]
 
-{x for x in range(10)}
+{x for x in range(10)} #tworzenie zbioru
 
 {x // 2 for x in range(10)}
 
-{x: x * x for x in range(10) if x % 2 == 0}
+{x: x * x for x in range(10) if x % 2 == 0} #dictionary comprehension
 
