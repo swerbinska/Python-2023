d3f0117f875ceaf32045f629e12f0ed75d5d8f7c
SWW <sylwia.werbinska@pwr.edu.pl>
2023-12-13 10:41:15 +0100

    uzupełnione

--- 12_Funkcje/funkcja 5.py
+++ 12_Funkcje/funkcja 5.py
@@ -2,12 +2,12 @@ def _slownie999(n, jednostki=['metr', 'metry', 'metrow']):
     nazwy_jednosci = {0: "", 1: "jeden", 2: "dwa", 3: "trzy", 4: "cztery", 5: "pięć", 6: "sześć", 7: "siedem",
                       8: "osiem", 9: "dziewięć"}
     nazwy_dziesiatki = {0: "", 10: "dziesiec", 20: "dwadziescia", 30: "trzydziesci", 40: "czterdziesci",
-                    50: "pięćdziesiat", 60: "sześćdziesiat", 70: "siedemdziesiat", 80: "osiemdziesiat",
-                    90: "dziewięćdziesiat"}
+                        50: "pięćdziesiat", 60: "sześćdziesiat", 70: "siedemdziesiat", 80: "osiemdziesiat",
+                        90: "dziewięćdziesiat"}
     nazwy_setki = {0: "", 100: "sto", 200: "dwiescie", 300: "trzysta", 400: "czterysta", 500: "pięćset",
-               600: "sześćset", 700: "siedemset", 800: "osiemset", 900: "dziewięćset"}
+                   600: "sześćset", 700: "siedemset", 800: "osiemset", 900: "dziewięćset"}
     nazwy_nastki = {0: "", 11: "jedenascie", 12: "dwanascie", 13: "trzynascie", 14: "czternascie", 15: "piętnascie",
-                16: "szesnascie", 17: "siedemnascie", 18: "osiemnascie", 19: "dziewiętnascie"}
+                    16: "szesnascie", 17: "siedemnascie", 18: "osiemnascie", 19: "dziewiętnascie"}
 
     ret = []
 
@@ -39,21 +39,25 @@ def _slownie999(n, jednostki=['metr', 'metry', 'metrow']):
 def _sklej(lista):
     return [t[1] for t in lista]
 
+
 def slownie(n):
-    jednosci = n %1000
-    tysiace = n //1000 % 1000
+    jednosci = n % 1000
+    tysiace = n // 1000 % 1000
     miliony = n // 1_000_000 % 1000
     miliardy = n // 1_0000_000_000 % 1000
+    tryliardy = n // 1_000_000_000_000 % 1000
 
     ret = []
+    if tryliardy:
+        ret+=(_slownie999(tryliardy, jednostki=['tryliard', 'tryliardy', 'tryliardow']))
     if miliardy:
-        ret+=(_slownie999(miliardy, jednostki=['miliard', 'miliardy', 'miliardow']))
+        ret += (_slownie999(miliardy, jednostki=['miliard', 'miliardy', 'miliardow']))
     if miliony:
-        ret+=(_slownie999(miliony, jednostki=['milion', 'miliony', 'milionow']))
+        ret += (_slownie999(miliony, jednostki=['milion', 'miliony', 'milionow']))
     if tysiace:
-        ret+=(_slownie999(tysiace, jednostki=['tysiac', 'tysiace', 'tysiecy']))
+        ret += (_slownie999(tysiace, jednostki=['tysiac', 'tysiace', 'tysiecy']))
     if jednosci:
-        ret+=(_slownie999(jednosci, jednostki=['', '', '']))
+        ret += (_slownie999(jednosci, jednostki=['', '', '']))
 
     return " ".join(_sklej(ret)).strip()
 
@@ -74,3 +78,4 @@ print(slownie(123_345))
 print(slownie(123_345_789))
 print(slownie(123_345_789_908))
 print(slownie(123_100_000_000))
+print(slownie(300_123_100_000_000))
