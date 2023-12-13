8e6c1700b6db8568df48dcb31ec4f02a9e6cc8f7
SWW <sylwia.werbinska@pwr.edu.pl>
2023-12-13 16:02:23 +0100

    uzupełnione

---
+++ 15_Pliki_tekstowe/przyklad.py
@@ -0,0 +1,12 @@
+import csv
+import sys
+
+data = []
+with open(sys.argv[1]) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        data.append(row)
+
+print(data)
+
+print(sum([float(x['Budget']) for x in data if x['Director'] == sys.argv[2]]))
