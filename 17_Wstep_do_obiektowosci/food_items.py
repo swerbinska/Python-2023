cc03f10b201e6791b752179dc9dd047eb9115a95
SWW <sylwia.werbinska@pwr.edu.pl>
2023-12-14 14:47:06 +0100

    uzupełnione

--- 17_Wstep_do_obiektowosci/food_items.py
+++ 17_Wstep_do_obiektowosci/food_items.py
@@ -2,14 +2,14 @@ import csv
 from pprint import pprint
 
 
-class FoodItem:
+class FoodItem: #tu jeszcze przyklad na plik csv a to samo w rodziale 19 ale na bazie danych
     def __init__(self, item_id, item_name, price):
         self.id = int(item_id)
         self.name = item_name
         self.price = float(price)
 
     def __repr__(self):
-        return f'FoodItem({self.id}, "{self.name}", {self.price})'
+        return f'FoodItem({self.id}, "{self.name}", {self.price})' #tak beda wyswietlane tresci
 
 
 food_list = []
@@ -18,6 +18,6 @@ with open('foods.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         print(row)
-        food_list.append(FoodItem(item_id=row['id'], item_name=row['name'], price=row['price']))
+        food_list.append(FoodItem(item_id=row['id'], item_name=row['name'], price=row['price'])) #tworzymy liste obiektów ktore inicjalizujemy funkcją init
 
-pprint(food_list)
+pprint(food_list) #i mamy liste
